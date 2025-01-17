53b65161a280808c51a60b2299a4127b4c914760
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2025-01-17 20:54:25 +0700

    s3 setting updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_STORAGE_BUCKET_NAME = 'django-storage8'
+AWS_S3_REGION_NAME = 'ap-southeast-1'  # Region ของ S3 Bucket
+AWS_QUERYSTRING_AUTH = False
