a2be0f035f0ceddbeda11d289c3a6ea3fe915407
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2025-01-17 23:53:07 +0700

    remove models

--- myapi/models.py
+++ myapi/models.py
@@ -1,9 +1,9 @@
-from django.db import models
+# from django.db import models
 
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/') 
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+# class Image(models.Model):
+#     image = models.ImageField(upload_to='images/') 
+#     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
 # Create your models here.
