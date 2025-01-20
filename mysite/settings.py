117c79226b402d40be98436456c3ecd3039c799c
Dara <dara@Daras-MacBook-Air.local>
2025-01-20 22:02:53 +0700

    modify s3 configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_STORAGE_BUCKET_NAME = 'django-storage8'
-AWS_S3_REGION_NAME = 'ap-southeast-1'  # Region ของ S3 Bucket
+AWS_STORAGE_BUCKET_NAME = 'django-bucket1212 '
+AWS_S3_REGION_NAME = 'ap-southeast-2'  # Region ของ S3 Bucket
 AWS_QUERYSTRING_AUTH = False
