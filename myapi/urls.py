8a451629f24b6e70f7b23f6e1255c93967e07695
Dara <dara@Daras-MacBook-Air.local>
2025-01-25 03:15:40 +0700

    feat: add login api , and authentication by AD

--- myapi/urls.py
+++ myapi/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.hello_world_view, name='hello_world'),
-    path('upload-images/',views.upload_image,name='upload_image')
+    path('upload-images/',views.upload_image,name='upload_image'),
+    path('user_login/',views.user_login, name='user_login')
 ]
