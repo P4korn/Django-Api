2098776b01d5b040150b0016667e21e49ae6b362
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2025-03-06 10:03:18 +0700

    updated path

--- myapi/urls.py
+++ myapi/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('hello', views.hello_world_view, name='hello_world'),
     path('upload-images',views.upload_image,name='upload_image'),
     path('user_login',views.user_login, name='user_login')
+
 ]
